6a5804e76a514e8ee6e997ec8d9e095d12c08930
David Guzmán <david_guzmanr@ciencias.unam.mx>
2021-06-08 19:08:26 -0500

    Add training.py

---
+++ Notebooks/task-1/utils/training.py
@@ -0,0 +1,197 @@
+from .preprocessing import preprocess
+
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm, trange
+
+from os import listdir
+from os.path import isfile, join
+import re
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, random_split
+import torch.optim as optim
+import torch.nn.functional as F
+from torchinfo import summary
+
+from transformers import BertTokenizer, BertModel
+
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, f1_score
+
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Reproducibility
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)
+torch.backends.cudnn.deterministic = True
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+def infer(model, loader_test):
+    """
+    Returns the prediction of a model in a dataset.
+
+    Parameters
+    ----------
+    model: PyTorch model
+    
+    loader_test: PyTorch DataLoader.
+
+    Returns
+    -------
+    tuple
+        y_true and y_pred 
+    """
+    model.eval()
+    
+    ys, ys_hat = [], []
+    for ids, masks, y_true in loader_test:
+        ids = ids.to(device)
+        masks = masks.to(device)
+        y_true = y_true.to(device)
+
+        y_hat = model(ids, masks)
+
+        loss = F.cross_entropy(y_hat, y_true)
+
+        y_pred = torch.argmax(y_hat, dim=1)
+
+        ys.extend(y_true.cpu().numpy().tolist() )
+        ys_hat.extend(y_pred.cpu().numpy().tolist())
+        
+    return ys, ys_hat
+
+def test(model, loader_test):
+    """
+    Returns the accuracy of a model in a dataset.
+
+    Parameters
+    ----------
+    model: PyTorch model
+    
+    loader_test: PyTorch DataLoader.
+
+    Returns
+    -------
+    float
+        Accuracy of the model.
+    """
+    model.eval()
+    
+    accs = []    
+    for ids, masks, y_true in loader_test:
+        ids = ids.to(device)
+        masks = masks.to(device)
+        y_true = y_true.to(device)
+        
+        y_hat = model(ids, masks)
+        
+        y_pred = torch.argmax(y_hat, 1)
+        
+        acc = (y_true == y_pred).type(torch.float32).mean()
+        
+        accs.append(acc.item())
+        
+    return np.mean(accs)
+        
+
+def train(model, loader_train, loader_test, epochs, lr=1e-5):
+    """
+    Trains a model.
+
+    Parameters
+    ----------
+    model: PyTorch model.
+    
+    loader_train: PyTorch DataLoader.
+    
+    loader_test: PyTorch DataLoader.
+    
+    epochs: int
+        Number of epochs to train the model
+
+    lr: float, default=1e-5
+        Learning rate for the optimizer.
+
+    Returns
+    -------
+    tuple
+        Tuple with the accuracy in the train and test dataset for each epoch.
+    """
+    opt = optim.Adam(model.parameters(), lr=lr)
+    
+    accs_train, accs_test = [], []    
+    for epoch in range(epochs):
+        model.train()
+        
+        accs = []
+        for ids, masks, y_true in tqdm(loader_train):
+            opt.zero_grad()
+
+            ids = ids.to(device)
+            masks = masks.to(device)
+            y_true = y_true.to(device)
+            
+            y_hat = model(ids, masks)
+            
+            loss = F.cross_entropy(y_hat, y_true)
+            
+            y_pred = torch.argmax(y_hat, dim=1)
+            
+            acc = (y_true == y_pred).type(torch.float32).mean()
+            accs.append(acc.item())
+            
+            loss.backward()
+            opt.step()
+        
+        acc_train = np.mean(accs)
+        accs_train.append(acc_train)
+        
+        acc_test = test(model, loader_test)
+        accs_test.append(acc_test)
+        
+        print(f'Epoch {epoch + 1}: train acc:{acc_train} test acc:{acc_test}')
+        
+    return accs_train, accs_test
+
+berttokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-uncased')
+def predict_sexism(model, text, label=True):
+    """
+    Predict if there is sexism present in the text.
+
+    Parameters
+    ----------
+    model: PyTorch model.
+
+    text: str
+        Text to analyze.
+
+    label: boolean, default=False
+        If True if will return the name of the label instead of the index.
+
+    Returns:
+    int or str
+        Index or label of the prediction.  
+    """
+    labels_dict = {0: 'non-sexist', 1: 'sexist'}
+
+    # Apply the same preprocessing
+    text = preprocess(text)
+
+    model.eval()
+
+    berttokenizer_dict = berttokenizer(text, max_length=128, padding='max_length', truncation=True, return_tensors='pt')
+    input_ids = berttokenizer_dict['input_ids'].to(device)
+    attention_mask = berttokenizer_dict['attention_mask'].to(device)
+
+    if label:
+        return labels_dict[torch.argmax(model(input_ids, attention_mask)).item()]     
+    else:
+        return torch.argmax(model(input_ids, attention_mask))      
